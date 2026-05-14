55a3479b9a317b8f3b02f7ca2c201ff8ec6415ce
unknown <youssef.wael9906@gmail.com>
2026-05-14 20:18:07 +0300

    Add Product Deatails Page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['172.29.32.1'],
+  // allowedDevOrigins: ['172.29.32.1'],
+  allowedDevOrigins: ['192.168.1.15'],
 };
 
 export default nextConfig;
